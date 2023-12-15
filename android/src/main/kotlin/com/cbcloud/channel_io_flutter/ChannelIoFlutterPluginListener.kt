61fea222e6f8ddb0b6df32c65d2e4b4b1828d2f3
YukiKitaike716 <y.kitaike716@cb-cloud.com>
2023-12-15 18:55:01 +0900

    fix onBadgeChanged

--- android/src/main/kotlin/com/cbcloud/channel_io_flutter/ChannelIoFlutterPluginListener.kt
+++ android/src/main/kotlin/com/cbcloud/channel_io_flutter/ChannelIoFlutterPluginListener.kt
@@ -30,7 +30,7 @@ class ChannelIoFlutterPluginListener : ChannelPluginListener {
 
     override fun onFollowUpChanged(p0: Map<String, String>) {}
 
-    override fun onBadgeChanged(p0: Int) {
+    override fun onBadgeChanged(p0: Int, p1: Int) {
         unreadEventHandler.eventSink?.success(p0)
     }
 
